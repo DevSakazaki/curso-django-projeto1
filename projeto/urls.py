4f4cef0926ccb8e40145763bf5b2caae8a4a4189
SergioJr <sergiojunior.dms@gmail.com>
2023-08-04 11:21:51 -0300

    request & Response

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+# Http Request
+
+
+def my_view(request):
+    return HttpResponse('Hello World, Hello Django!')
+    # Tem que retornar HTTP Response
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('sobre/', my_view)
 ]
